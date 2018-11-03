227ed727c2e3469924991f6e57f9b2337bf29b22
Yrwynmyr <42325411+Yrwynmyr@users.noreply.github.com>
2018-11-03 06:57:49 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Pi from './24pi.png';
 import './App.css';
 
-      function myFunction(x) {
-    x.style.background = "yellow";
-}
+
+
+    var myConfig = {
+      "type":"ring",
+      "title":{
+        "text":"Donut Chart"
+      },
+      "plot":{
+        //Use the "slice" attribute to adjust the size of the donut ring.
+      },
+      "series":[
+        {"values":[59]},
+        {"values":[55]},
+        {"values":[30]},
+        {"values":[28]},
+        {"values":[15]}
+      ]
+    };
+     
+  
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App"><head>
+      
+<script src= "https://cdn.zingchart.com/zingchart.min.js"></script>
+		<script> zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
+		ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9","ee6b7db5b51705a13dc2339db3edaf6d"];</script>
+       </head> <header className="App-header"><h1>DowsingStar</h1>
+       <div id='myChart'></div>
+       
+        
 	
-        <img src={Pi} className="Pi" alt="pichart"/>
         </header>
         
- <input type="text" onfocus="myFunction(this)"></input>
+      
       </div>
      
     );
